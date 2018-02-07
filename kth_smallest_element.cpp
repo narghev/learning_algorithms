17cc024172b3c7947f123a2a574fb055ddabb211
narghev <n.ghevandiani@gmail.com>
2018-02-08 01:27:35 +0400

    finding medians

--- kth_smallest_element.cpp
+++ kth_smallest_element.cpp
@@ -1,23 +1,36 @@
 #include <iostream>
-#include <vector>
+#include <algorithm>
+#include<climits>
 
 using namespace std;
 
-int sort_and_median(int array[], int range){
-  cout<<array<<endl;
+int sort_and_return_median(int array[], int range){
   sort(array, array+range);
   return array[range/2];
-} 
+}
 
-int kth_smallest_element(int array[], int length){
-  int medians[length/5];
-  for (int i=0; i<length/5; i++){
-    medians[i] = sort_and_median(array+i, 5);
-    cout<<"median"<<medians[i]<<endl;
+int kth_smallest_element(int array[], int left, int right, int k){
+  int number_of_elements = right-left+1;
+
+  if (k<0 || k>number_of_elements){
+    return INT_MAX;
   }
+
+  int number_of_medians = (number_of_elements/5) + (int)(number_of_elements%5!=0);
+  int medians[number_of_medians];
+  for (int i=0; i<number_of_medians; i++){
+    medians[i] = sort_and_return_median(array+left+i*5,
+      i==number_of_medians-2 ? number_of_elements%5 : 5);
+    cout<<medians[i]<<endl;
+  }
+
+  return 0;
 }
 
 int main(){
-  int array[] = {3,2,1,5,5,6,7,33,123,77};
-  kth_smallest_element(array, array.length());
+  int array[] = {3,2,1,5,89,6,7,33,123,77,44};
+  int size = sizeof(array)/sizeof(array[0]);
+  kth_smallest_element(array, 0, size-1, 1);
+
+  return 0;
 }
