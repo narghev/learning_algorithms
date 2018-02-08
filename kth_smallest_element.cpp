94dd57d749fed6ce2d0d74fcd0d4054fb2008264
narghev <n.ghevandiani@gmail.com>
2018-02-09 00:30:15 +0400

    WIP commit

--- kth_smallest_element.cpp
+++ kth_smallest_element.cpp
@@ -1,8 +1,31 @@
 #include <iostream>
 #include <algorithm>
-#include<climits>
+#include <climits>
 
 using namespace std;
+
+int partition(int array[], int left, int right, int pivot){
+  int i = left;
+
+  for (i; i<right; i++){
+    if(array[i] == pivot){
+      break;
+    }
+  }
+
+  int pivot_index = i;
+  swap(array[pivot_index], array[right]);
+
+  i=left;
+  for (int j=left; j<right; j++){
+    if(array[j]<=array[right]){
+      swap(array[j], array[i]);
+      i++;
+    }
+  }
+  swap(array[i], array[right]);
+  return i;
+}
 
 int sort_and_return_median(int array[], int range){
   sort(array, array+range);
@@ -11,11 +34,6 @@ int sort_and_return_median(int array[], int range){
 
 int kth_smallest_element(int array[], int left, int right, int k){
   int number_of_elements = right-left+1;
-
-
-  // for (int j=0; j<number_of_elements; j++)
-  //   cout<<array[j]<<" ";
-  // cout<<endl;
 
   if (k<0 || k>=number_of_elements){
     return INT_MAX;
@@ -32,13 +50,23 @@ int kth_smallest_element(int array[], int left, int right, int k){
   int median_of_medians = (number_of_medians == 1) ? medians[0] : kth_smallest_element(medians, 0,
     number_of_medians-1, number_of_medians/2);
 
-  return 0;
+  int position_of_med_of_meds = partition(array, left, right, median_of_medians);
+
+  if (position_of_med_of_meds-left+1 == k){
+    return array[k];
+  }
+  else if (position_of_med_of_meds < k){
+    int new_k = k-position_of_med_of_meds+left-1;
+    return kth_smallest_element(array, position_of_med_of_meds+1, right, new_k);
+  }
+  
+  return kth_smallest_element(array, left, position_of_med_of_meds-1, k);
 }
 
 int main(){
   int array[] = {3,2,1,5,89,6,7,33,123,77,44};
   int size = sizeof(array)/sizeof(array[0]);
-  kth_smallest_element(array, 0, size-1, 1);
+  cout<<kth_smallest_element(array, 0, size-1, 3)<<endl;
 
   return 0;
 }
